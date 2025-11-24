3b497701c4c0806103f6ec705854a56df0c999d4
Veeti Martinmäki <181813689+veetimar@users.noreply.github.com>
2025-11-24 19:42:47 +0200

    Add viewing books in bibtex format

--- src/util.py
+++ src/util.py
@@ -1,4 +1,18 @@
 class UserInputError(Exception):
     pass
 
-# helper functions
+
+def make_bibtex(book):
+    tex = f"@book{{{book.id},\n"
+    if book.author:
+        tex += f"  author = {{{book.author}}},\n"
+    if book.title:
+        tex += f"  title = {{{book.title}}},\n"
+    if book.year:
+        tex += f"  year = {{{book.year}}},\n"
+    if book.publisher:
+        tex += f"  publisher = {{{book.publisher}}},\n"
+    if book.address:
+        tex += f"  address = {{{book.address}}},\n"
+    tex += "}"
+    return tex
