4fa0ce775508abc021db2632d6c57569ef66a325
Aaron <119842456+kahkaar@users.noreply.github.com>
2025-12-11 09:31:25 +0200

    Remove feature toggle

--- src/index.py
+++ src/index.py
@@ -3,7 +3,3 @@ from util import set_session
 
 if __name__ == "__main__":
     app.run(port=5001, host="0.0.0.0", debug=True)
-
-    # Enable or disable tag and category search feature
-    TC_SEARCH = True
-    set_session("tc-search", TC_SEARCH)
