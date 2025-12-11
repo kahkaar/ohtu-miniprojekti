60017892a5f762ef263356a4808f59d9d6a03da3
Aaron <119842456+kahkaar@users.noreply.github.com>
2025-12-11 09:05:59 +0200

    Add toggle for category/tag search

--- src/index.py
+++ src/index.py
@@ -1,4 +1,9 @@
 from app import app
+from util import set_session
 
 if __name__ == "__main__":
     app.run(port=5001, host="0.0.0.0", debug=True)
+
+    # Enable or disable tag and category search feature
+    tc_search = False
+    set_session("tc-search", tc_search)
